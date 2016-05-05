30ed7d35b482323d4622e66a3446264d1bc15eec
Furkan Yesiler <myesiler@ku.edu.tr>
2016-05-05 21:42:55 +0300

    no message

---
+++ src/rhythmic/RhythmicHelper.java
@@ -0,0 +1,143 @@
+package rhythmic;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import utilities.AudioUtilities;
+import utilities.ThresholdFunction;
+import utilities.WaveReader;
+
+public class RhythmicHelper {
+	
+	public static final int HISTORY_SIZE = 50;
+	public static final float[] multipliers = { 2f, 2f, 2f };
+	ArrayList<Integer> peakPro;
+	int[] a1; 
+	int[] a2;
+	double[] a3;
+	double[] a4;
+	int numOfPeaks;
+	double totalMeas;
+	ArrayList<Integer> locOfPeaks;
+	double dist;
+
+	public RhythmicHelper(String name){
+		WaveReader reader = new WaveReader();
+		File soundFile = new File(name.concat(".wav"));
+		reader.changeWave(soundFile);
+		float[] org=reader.getFloatData();
+
+		float[][] chunked = AudioUtilities.chunkArray(org,1024);
+
+		FFT fft = new FFT( 1024, 44100 );
+		fft.window( FFT.HAMMING );
+		float[] samples = new float[1024];
+		float[] spectrum = new float[1024 / 2 + 1];
+		float[] lastSpectrum = new float[1024 / 2 + 1];
+		List<Float> spectralFlux = new ArrayList<Float>( );
+		List<Float> hop = new ArrayList<Float>();
+
+		//BufferedWriter wr = new BufferedWriter(new FileWriter(new File("out2.txt")));
+		for(int j=0;j<chunked.length;j++){
+			//while( decoder.readSamples( samples ) > 0 )
+			//{			
+			fft.forward( chunked[j] );
+			System.arraycopy( spectrum, 0, lastSpectrum, 0, spectrum.length ); 
+			System.arraycopy( fft.getSpectrum(), 0, spectrum, 0, spectrum.length );
+
+			float flux = 0;
+			for( int i = 0; i < spectrum.length; i++ )	
+			{
+				float value = (spectrum[i] - lastSpectrum[i]);
+				flux += value < 0? 0: value;
+			}
+			spectralFlux.add( flux );
+			hop.add(flux*11/12);
+			//}		
+		}
+		List<List<Float>> thresholds = new ArrayList<List<Float>>( );
+
+		List<Float> threshold = new ThresholdFunction( HISTORY_SIZE, multipliers[0]).calculate( spectralFlux );
+		thresholds.add( threshold );
+
+		//Plot plot = new Plot( "Spectral Flux", 1024, 512 );
+		//plot.plot(spectralFlux, 1, 0, false, Color.red);
+		//plot.plot(thresholds.get(0), 1,0,true,Color.blue);
+		ArrayList<Integer> peak = new ArrayList<Integer>();
+		this.peakPro = new ArrayList<Integer>();
+		for(int i=0;i<spectralFlux.size();i++){
+			if(spectralFlux.get(i)>thresholds.get(0).get(i)+5){
+				peak.add(1);
+			}else{
+				peak.add(0);
+			}
+		}
+		int count =0;
+		for(int i=0;i<peak.size();i++){
+			if(count==0 && peak.get(i)==1){
+				count++;
+			}
+			if(count==1 && peak.get(i)==1){
+				for(int j=0;j<peak.size();j++){
+					if((i+j)<peak.size())
+						peakPro.add(peak.get(i+j));
+				}
+				count++;
+			}
+		}
+		a1 = new int[peakPro.size()];
+		
+		for(int i=0; i<peakPro.size();i++){
+			a1[i]=peakPro.get(i);
+		}
+		this.numOfPeaks=0;
+		locOfPeaks = new ArrayList<Integer>();
+		int flag=0;
+		for(int i=0;i<a1.length;i++){
+			if(i!=0){
+				if(a1[i]==1 && a1[i-1]!=1){
+					numOfPeaks++;
+					locOfPeaks.add(i);
+				}
+			}
+				
+			if(a1[i]==1)
+				flag=i;
+		}
+		
+		a2 = new int[flag+1];
+		
+		for(int i=0; i<a2.length;i++){
+			a2[i]=a1[i];
+		}
+		
+		a3 = new double[numOfPeaks-1];
+		for(int i=0; i<numOfPeaks-1;i++){
+			a3[i]=locOfPeaks.get(i+1)-locOfPeaks.get(i);
+			//System.out.println(a3[i]);
+		}
+		totalMeas=0;
+		a4 = new double[a3.length];
+		for(int i=0;i<a3.length;i++){
+			double first = a3[0];
+			a4[i]=a3[i]/first;
+			System.out.println(a4[i]);
+			totalMeas=totalMeas+a4[i];
+		}
+	}
+
+	public double[] getA4() {
+		return a4;
+	}
+
+	public int getNumOfPeaks() {
+		return numOfPeaks;
+	}
+
+	public double getTotalMeas() {
+		return totalMeas;
+	}
+	
+	
+}
--- src/rhythmic/RhythmicMain.java
+++ src/rhythmic/RhythmicMain.java
@@ -1,5 +1,181 @@
 package rhythmic;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import utilities.AudioUtilities;
+import utilities.ThresholdFunction;
+import utilities.WaveReader;
+
+
 public class RhythmicMain {
 
+	private AudioFormat audioFormat;
+	private TargetDataLine targetDataLine;
+	public static final int HISTORY_SIZE = 50;
+	public static final float[] multipliers = { 2f, 2f, 2f };
+	ArrayList<Integer> peakPro;
+	int[] a1; 
+	int[] a2;
+	double[] a3;
+	double[] a4;
+	int numOfPeaks;
+	double totalMeas;
+	ArrayList<Integer> locOfPeaks;
+	double dist;
+	String exName;
+	ArrayList<Integer> al1 = new ArrayList<Integer>();
+	ArrayList<Integer> al2 = new ArrayList<Integer>();
+
+	public RhythmicMain(){
+		exName = "rhy1";
+
+	}
+
+	public void captureAudio(){
+		try{
+			//Get things set up for capture
+			audioFormat = getAudioFormat();
+			DataLine.Info dataLineInfo =
+					new DataLine.Info(
+							TargetDataLine.class,
+							audioFormat);
+			targetDataLine = (TargetDataLine)
+					AudioSystem.getLine(dataLineInfo);
+
+			//Create a thread to capture the microphone
+			// data into an audio file and start the
+			// thread running.  It will run until the
+			// Stop button is clicked.  This method
+			// will return after starting the thread.
+			new CaptureThread().start();
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}//end catch
+	}
+
+	public AudioFormat getAudioFormat(){
+		float sampleRate = 44100F;
+		//8000,11025,16000,22050,44100
+		int sampleSizeInBits = 16;
+		//8,16
+		int channels = 1;
+		//1,2
+		boolean signed = true;
+		//true,false
+		boolean bigEndian = false;
+		//true,false
+		return new AudioFormat(sampleRate,
+				sampleSizeInBits,
+				channels,
+				signed,
+				bigEndian);
+	}
+
+	class CaptureThread extends Thread{
+		public void run(){
+			AudioFileFormat.Type fileType = null;
+			File audioFile = null;
+
+			//Set the file type and the file extension
+			// based on the selected radio button.
+
+			fileType = AudioFileFormat.Type.WAVE;
+			audioFile = new File("recRhy.wav");
+			//end if
+
+			try{
+				targetDataLine.open(audioFormat);
+				targetDataLine.start();
+				AudioSystem.write(
+						new AudioInputStream(targetDataLine),
+						fileType,
+						audioFile);
+			}catch (Exception e){
+				e.printStackTrace();
+			}//end catch
+
+		}//end run
+	}
+	
+	public void playEx() throws IOException{
+		String gongFile = this.exName.concat(".wav");
+	    InputStream in = new FileInputStream(gongFile);
+	 
+	    // create an audiostream from the inputstream
+	    AudioStream audioStream = new AudioStream(in);
+	 
+	    // play the audio clip with the audioplayer class
+	    AudioPlayer.player.start(audioStream);
+	}
+
+	public void analyze(){
+		RhythmicHelper rh1 = new RhythmicHelper(this.exName);
+		RhythmicHelper rh2 = new RhythmicHelper("recRhy");
+		
+		double totalM1 = rh1.getTotalMeas();
+		double totalM2 = rh2.getTotalMeas();
+		
+		int base1 = (int) (400/totalM1);
+		int base2 = (int) (400/totalM2);
+		
+		int peak1 = rh1.getA4().length;
+		int peak2 = rh2.getA4().length;
+		
+		int[] ref1 = new int[400];
+		int[] ref2 = new int[400];
+		
+		double[] peaks1 = rh1.getA4();
+		double[] peaks2 = rh2.getA4();
+		
+		ref1[0]=1;
+		for(int i=0;i<peak1-1;i++){
+			double num=0;
+			for(int j=0; j<=i;j++){
+				num=num+peaks1[j];
+			}
+			ref1[(int) (num*base1)]=1;
+		}
+		ref2[0]=1;
+		for(int i=0;i<peak2-1;i++){
+			double num=0;
+			for(int j=0; j<=i; j++){
+				num=num+peaks2[j];
+			}
+			ref2[(int) (num*base2)]=1;
+		}
+		
+		for(int i=0;i<ref1.length;i++){
+			this.al1.add(ref1[i]);
+		}
+		
+		for(int i=0;i<ref2.length;i++){
+			this.al2.add((int) ref2[i]);
+		}
+	}
+
+	public ArrayList<Integer> getAl1() {
+		return al1;
+	}
+
+	public ArrayList<Integer> getAl2() {
+		return al2;
+	}
+	
+	
 }
